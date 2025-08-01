d1d992c70fb1761ffd44ef682d262747b0d482c9
jitendra parihar <jitendraparihar457@.com>
2025-08-01 18:07:26 +0530

    my first commit

---
+++ dsa/FindLongestString.js
@@ -0,0 +1,19 @@
+// find the longest string in a array
+
+let array = ["applee", "orange", "mango", "pea"];
+
+const findLongestString = (array) => {
+  let longestString = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let currentString = array[i];
+    if (currentString.length > longestString.length) {
+      longestString = currentString;
+      
+    }
+  }
+  return longestString;
+};
+
+let result = findLongestString(array);
+console.log(result);
